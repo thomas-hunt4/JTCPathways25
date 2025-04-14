57caea1b0c0ac5073c00409c4b51c1c87739a3b3
Thomas Hunt <thun366@wgu.edu>
2025-04-15 00:17:38 +0200

    saving work before starting class

---
+++ modimport.py
@@ -0,0 +1,11 @@
+# Examples of importing in Python
+
+from JTCPATHWAYS25 import importing
+
+def main () -> None:
+    # Entrypoint of program
+    print(importing.powerful(2, 4))
+
+
+if __name__ == '__main__":
+    main()
